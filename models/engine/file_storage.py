0fa30f4926fdc039aba5eea9b927e6c6f4157d93
95ktsmith <95ktsmith@gmail.com>
2020-06-26 21:30:41 +0000

    Works, but importing inside a try/except to avoid the error has me shook.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,23 +2,27 @@
 """ File Storage Module """
 
 import json
-from models.base_model import BaseModel
+
 
 class FileStorage():
-    """ File storage class """
+    """ File storage class
+    """
     __file_path = "jsonfile.json"
     __objects = {}
 
     def all(self):
-        """ Returns the __objects dictionary """
+        """ Returns the __objects dictionary
+        """
         return self.__objects
 
     def new(self, obj):
-        """ Adds an object to the __objects dictionary """
+        """ Adds an object to the __objects dictionary
+        """
         self.__objects[str(type(obj).__name__) + "." + obj.id] = obj
 
     def save(self):
-        """ Serializes __objects to the JSON file (path: __file_path) """
+        """ Serializes __objects to the JSON file (path: __file_path)
+        """
         list_of_dicts = []
         for key, value in self.__objects.items():
             list_of_dicts.append(value.to_dict())
@@ -26,10 +30,15 @@ class FileStorage():
             file.write(json.dumps(list_of_dicts))
 
     def reload(self):
-        """ Deserializes the JSON file to __objects if the file exists """
-        with open(self.__file_path, "r") as file:
-            list_of_dicts = json.loads(file.read())
-        self.__objects = {}
-        for obj_dict in list_of_dicts:
-            if obj_dict['__class__'] == "BaseModel":
-                self.new(BaseModel(**obj_dict))
+        """ Deserializes the JSON file to __objects if the file exists
+            I don't feel good about this but it works
+        """
+        try:
+            with open(self.__file_path, "r") as file:
+                list_of_dicts = json.loads(file.read())
+            from models.base_model import BaseModel
+            for obj_dict in list_of_dicts:
+                if obj_dict['__class__'] == "BaseModel":
+                    self.new(BaseModel(**obj_dict))
+        except:
+            pass
