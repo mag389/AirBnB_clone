5410ce9f6087a25c78737e0ffae76534299427d6
mag389 <mag389@cornell.edu>
2020-06-29 20:09:23 +0000

    tests string and dict methods

--- tests/tests_base1.py
+++ tests/tests_base1.py
@@ -20,3 +20,20 @@ class TestBase(unittest.TestCase):
         self.assertIsInstance(b.updated_at, dt)
         self.assertNotIsInstance(b.id, uuid.UUID)
         self.assertIsInstance(b.id, str)
+
+    def test_bm2(self):
+        """second set of testing for baseModel"""
+        bm2 = BaseModel()
+#        #bm2.id = "1"
+        strep = "[{}] ({}) {}".format(type(bm2).__name__, bm2.id, bm2.__dict__)
+        self.assertEqual(str(bm2), strep)
+
+    def test_bm3(self):
+        """testing to_dict"""
+        bm3 = BaseModel()
+        cdict = {}
+        cdict['__class__'] = "BaseModel"
+        cdict["created_at"] = str(bm3.created_at.isoformat())
+        cdict["updated_at"] = str(bm3.updated_at.isoformat())
+        cdict["id"] = bm3.id
+        self.assertEqual(cdict, bm3.to_dict())
