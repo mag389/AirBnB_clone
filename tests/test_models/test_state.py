9d02ad4dcaa8d96f18169aa1e156ccbc3477619d
mag389 <mag389@cornell.edu>
2020-06-30 18:35:17 +0000

    adding state test file and pep8 for user

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
-""" unit testing file for Users """
+""" unit testing file for States """
 
 
 import unittest
 from models.base_model import BaseModel
-from models.user import User
+from models.state import State
 import uuid
 import json
 from datetime import datetime as dt
 
 
-class TestUser(unittest.TestCase):
-    """class for unit testing Users"""
+class TestState(unittest.TestCase):
+    """class for unit testing States"""
 
     def test_bm1(self):
         """basic test for attributes """
-        u = User()
+        u = State()
         self.assertIsInstance(u, BaseModel)
         self.assertIsInstance(u.created_at, dt)
         self.assertIsInstance(u.updated_at, dt)
         self.assertNotIsInstance(u.id, uuid.UUID)
         self.assertIsInstance(u.id, str)
 
-    def test_u1(self):
-        """ testing for user specific attributes """
-        u = User()
-        self.assertIsInstance(u.email, str)
-        self.assertIsInstance(u.password, str)
-        self.assertIsInstance(u.first_name, str)
-        self.assertIsInstance(u.last_name, str)
+    def test_s1(self):
+        """ testing for state specific attributes """
+        s = State()
+        self.assertIsInstance(s.name, str)
 
-    def test_u2(self):
+    def test_s2(self):
         """testing for serialization unsure as of now"""
-        u = User()
+        u = State()
